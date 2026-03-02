6dfdfd19c5b33c48079ff768903ca0b50c8f54f2
Hamza <hamzarezgui.education@gmail.com>
2026-03-02 21:34:10 +0100

    defining graph, prompts and states for the app

---
+++ agent/graph.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_groq import ChatGroq
+from prompts import *
+from states import *
+
+llm = ChatGroq(model="openai/gpt-oss-120b")
+
+user_prompt = "Create a simple calculator web application"
+
+prompt = planner_prompt(user_prompt)
+
+resp = llm.with_structured_output(Plan).invoke(prompt)
+
+print(resp)
